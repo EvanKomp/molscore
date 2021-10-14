e0799022aaf1a7216cbf66f779ddaee1383dd074
Evan Komp <evankomp@uw.edu>
2021-10-14 09:51:45 -0700

    test for init

--- tests/test___init__.py
+++ tests/test___init__.py
@@ -1,7 +1,9 @@
 """Checks global variables initialization."""
-
 import pytest
 
-# def test_globals():
-#     import molscore
-#     molscore._set_globals({'DEFAULT_DATABASE_ROOT': './new_location'})
+def test_globals(working_test_dir):
+    import molscore
+    molscore._set_globals({'DEFAULT_DATABASE_ROOT': f'{working_test_dir}/another_data'})
+    assert molscore.DEFAULT_HANDLER.root == f'{working_test_dir}/another_data',\
+        "Did not change default handler"
+    return
